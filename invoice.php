d5be5ffe45a3cf0497dab599615c57b0b98ce02c
Supalu1 <supaluck.erbim@gmail.com>
2018-12-31 21:49:30 +0700

    invoice

---
+++ invoice.php
@@ -0,0 +1,85 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<link href="css/invoice.css" rel="stylesheet">
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body p-0">
+                    <div class="row p-4">
+                        <div class="col-md-6">
+                            <img src="image/f-logo.jpg" alt="">
+                        </div>
+
+                        <div class="col-md-6 text-right">
+                            <p class="font-weight-bold mb-1">Invoice #550</p>
+                            <p class="text-muted">Due to: <?=date("Y/m/d")?></p>
+                        </div>
+                    </div>
+
+                    <hr class="my-5">
+
+                    <div class="row p-5">
+                        <div class="col-md-12">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th class="border-0 text-uppercase small font-weight-bold">ID</th>
+                                        <th class="border-0 text-uppercase small font-weight-bold">Item</th>
+                                        <th class="border-0 text-uppercase small font-weight-bold">Description</th>
+                                        <th class="border-0 text-uppercase small font-weight-bold">Quantity</th>
+                                        <th class="border-0 text-uppercase small font-weight-bold">Unit Cost</th>
+                                        <th class="border-0 text-uppercase small font-weight-bold">Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Software</td>
+                                        <td>LTS Versions</td>
+                                        <td>21</td>
+                                        <td>$321</td>
+                                        <td>$3452</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Software</td>
+                                        <td>Support</td>
+                                        <td>234</td>
+                                        <td>$6356</td>
+                                        <td>$23423</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Software</td>
+                                        <td>Sofware Collection</td>
+                                        <td>4534</td>
+                                        <td>$354</td>
+                                        <td>$23434</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div class="d-flex flex-row-reverse bg-dark text-white p-4">
+                         <div class="py-3 px-5 text-right">
+                            <div class="mb-2">Sub - Total amount</div>
+                            <div class="h2 font-weight-light">$32,432</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="text-light mt-5 mb-5 text-center small">by : <a class="text-light" target="_blank" href="http://totoprayogo.com">totoprayogo.com</a></div>
+
+</div>
+
+
